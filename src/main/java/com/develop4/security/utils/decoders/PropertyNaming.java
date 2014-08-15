faf72e6f3f52b9d9583179ea28eeabd57f597657
william_timpany <william.timpany@db.com>
2014-08-15 11:08:13 +0100

    xx

---
+++ src/main/java/com/develop4/security/utils/decoders/PropertyNaming.java
@@ -0,0 +1,33 @@
+/* 
+ * =============================================================================
+ * 
+ *  Copyright (c) 2014, The Develop4 Technologies Ltd (http://www.develop4.co.uk)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * =============================================================================
+ */
+package com.develop4.security.utils.decoders;
+
+final class PropertyNaming {
+	
+	public static final String PROP_PASSPHRASE 					= "passphrase";
+	public static final String PROP_PROVIDER_NAME 				= "providerName";
+	public static final String PROP_PROVIDER_CLASS_NAME 		= "providerClassName";
+	public static final String PROP_ALGORITHM_NAME 				= "algorithmName";
+	public static final String PROP_OBTENTION_ITERATIONS 		= "obtentionIterations";
+	public static final String PROP_SALT_GENERATOR_CLASS_NAME 	= "saltGeneratorClassName";
+	public static final String PROP_STRING_OUTPUT_TYPE 			= "stringOutputType";
+	public static final String PROP_DEBUG 						= "debug";
+
+}
